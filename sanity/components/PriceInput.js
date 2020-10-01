1437f9f825ee6ddd3d448a34765c64e15c72612e
A-J Roos <asjas@hey.com>
2020-10-01 09:01:57 +0200

    fix ref forwarding for PriceInput

--- sanity/components/PriceInput.js
+++ sanity/components/PriceInput.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import PatchEvent, { set, unset } from 'part:@sanity/form-builder/patch-event';
 
 function createPatchFrom(value) {
-  return PatchEvent.from(value === '' ? unset() : set(Number(value)))
+  return PatchEvent.from(value === '' ? unset() : set(Number(value)));
 }
 
 const formatMoney = Intl.NumberFormat('en-ZA', {
@@ -11,27 +10,25 @@ const formatMoney = Intl.NumberFormat('en-ZA', {
   currency: 'ZAR',
 }).format;
 
-export default function PriceInput({ type, value, onChange, inputComponent }) {
+function PriceInput({ type, value, onChange }, inputComponent) {
   return (
     <div>
-      <h2>{type.title} - {value ? formatMoney(value / 100) : null}</h2>
+      <h2>
+        {type.title}- {value ? formatMoney(value / 100) : ''}
+      </h2>
       <p>{type.description}</p>
-      <input type={type.name} value={value} onChange={event => onChange(createPatchFrom(event.target.value))} ref={inputComponent} />
+      <input
+        type={type.name}
+        value={value}
+        onChange={(event) => onChange(createPatchFrom(event.target.value))}
+        ref={inputComponent}
+      />
     </div>
-  )
+  );
 }
 
 PriceInput.focus = function () {
   this._inputElement.focus();
-}
+};
 
-PriceInput.propTypes = {
-  type: PropTypes.shape({
-    title: PropTypes.string,
-    description: PropTypes.string,
-    name: PropTypes.string,
-  }).isRequired,
-  value: PropTypes.number,
-  onChange: PropTypes.func.isRequired,
-  inputComponent: PropTypes.func.isRequired,
-}
+export default React.forwardRef(PriceInput);
